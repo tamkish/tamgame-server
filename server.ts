bc554bd9de79bbf8f4c0092d81062e6fcc39504a
Tamkish <martin@vrba.dev>
2024-10-09 19:17:33 +0200

    basic ws echo

--- server.ts
+++ server.ts
@@ -1,1 +1,30 @@
-console.log("test")
+import { WebSocketServer } from 'ws';
+
+const server = new WebSocketServer({port:8080});
+
+server.on("connection", socket => socket.send(JSON.stringify(socket)))
+
+
+/*
+const wss = new WebSocketServer({
+    port: 8080,
+    perMessageDeflate: {
+        zlibDeflateOptions: {
+            // See zlib defaults.
+            chunkSize: 1024,
+            memLevel: 7,
+            level: 3
+        },
+        zlibInflateOptions: {
+            chunkSize: 10 * 1024
+        },
+        // Other options settable:
+        clientNoContextTakeover: true, // Defaults to negotiated value.
+        serverNoContextTakeover: true, // Defaults to negotiated value.
+        serverMaxWindowBits: 10, // Defaults to negotiated value.
+        // Below options specified as default values.
+        concurrencyLimit: 10, // Limits zlib concurrency for perf.
+        threshold: 1024 // Size (in bytes) below which messages
+        // should not be compressed if context takeover is disabled.
+    }
+});*/
